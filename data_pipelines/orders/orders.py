14a23655a4aee3e2e67340221495d68c3bedaa0b
Matt Falcioni <mfalcioni1@gmail.com>
2022-07-04 18:55:56 -0400

    updates for shmovement

--- data_pipelines/orders/orders.py
+++ data_pipelines/orders/orders.py
@@ -15,13 +15,16 @@ import utils.shmovement as move
 
 def main():
     #order_detail = su.get_orders()
-    order_detail = su.get_orders(query_params = {'since_id' : 4722290491636})
+    order_detail = su.get_orders(query_params = {'created_at_min': '2022-05-01', 'created_at_max': '2022-06-01'})
     #TODO: Need to come up with an order list update flow.
     #TODO: Need to parse out the order header
     #TODO: Need to parse out the order detail
     order_detail.head()
     order_detail.columns
     order_detail.shape
+
+    order_detail['order_date'] = pd.to_datetime(order_header['created_at'])
+    order_detail['order_date'] = order_detail['order_date'].dt.date
     # unnesting json columns
     # for i in order_detail.columns:
     #     if order_detail[i].dtype == "object"
@@ -29,14 +32,14 @@ def main():
     customer = pd.json_normalize(order_detail['customer']).add_prefix('customer_')
     #TODO: Anonymize Customer
 
-    oh_cols = ['id', 'order_number', 'name', 'source_name', 
+    oh_cols = ['id', 'order_number', 'order_date', 'name', 'source_name', 
         'confirmed', 'processing_method', 'financial_status', 'fulfillment_status',
         'created_at', 'processed_at', 'closed_at',
         'subtotal_price', 'total_price', 'total_tax', 'current_total_price', 
         'total_discounts', 'discount_codes',
         'note', 'note_attributes', 'cancel_reason', 'updated_at', 'cancelled_at']
     order_header  = order_detail[oh_cols]
-    
+
     if bq.bq_table_exists(dataset_id = "orders", table_name = "order_header"):
         oh_write_disposition = "WRITE_TRUNCATE"
     else:
@@ -48,7 +51,9 @@ def main():
             gcs_path = f"{utils.load_config()['gcs-orders']}order-header",
             dataset = "orders",
             table_name = "order_header",
-            write_disposition = oh_write_disposition)
+            write_disposition = oh_write_disposition,
+            partition = True,
+            partition_by = "created_at")
 
 
 if __name__ == "__main__":
