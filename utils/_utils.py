590caab49a72f647038b39b62d8962c2ed1dbbf2
Matt Falcioni <mfalcioni1@gmail.com>
2022-07-04 18:55:37 -0400

    dropped updates check  and timestamp

--- utils/_utils.py
+++ utils/_utils.py
@@ -1,8 +1,6 @@
 # built in
 import json
 import os
-import datetime
-import re
 
 def load_config(path = None):
     if path == None:
@@ -33,21 +31,3 @@ def gcp_config(path = None):
     Service Account: {data_bot}.
     """
     )
-
-def date_stamp(name: str):
-    utc = datetime.datetime.utcnow()
-    s_utc = utc.strftime("%Y%m%d")
-    return f"{name}{s_utc}"
-
-def check_latest(api_res, gcs_iter):
-    newest = api_res[api_res['created_at'] == api_res['created_at'].max()]['created_at'].iloc[0]
-    latest_api = int(datetime.datetime.fromisoformat(newest).strftime("%Y%m%d"))
-    if gcs_iter:
-        r = re.compile("\d+")
-        latest_db = max([int(r.search(x).group(0)) for x in gcs_iter])
-        update_bool = latest_api > latest_db
-    else:
-        print("No files in GCS. Setting update to True.")
-        latest_db = None
-        update_bool = True
-    return latest_api, latest_db, update_bool
