eec17a9a92490ac34ee9b6aa3be87b5819f19e11
matthew-falcioni <mfalcioni1@gmail.com>
2023-05-11 21:50:38 -0400

    schmover needs rewriten to just do partitions

--- data_pipelines/orders/orders.py
+++ data_pipelines/orders/orders.py
@@ -16,24 +16,14 @@ import utils.shmovement as move
 def main():
     #order_detail = su.get_orders()
     order_detail = su.get_orders(query_params = {'created_at_min': '2022-05-01', 'created_at_max': '2022-06-01'})
-    #TODO: Need to come up with an order list update flow.
-    #TODO: Need to parse out the order header
-    #TODO: Need to parse out the order detail
-    order_detail.head()
-    order_detail.columns
-    order_detail.shape
-
-    order_detail['order_date'] = pd.to_datetime(order_header['created_at'])
-    order_detail['order_date'] = order_detail['order_date'].dt.date
-    # unnesting json columns
-    # for i in order_detail.columns:
-    #     if order_detail[i].dtype == "object"
+    
     billing_address = pd.json_normalize(order_detail['billing_address']).add_prefix('billing_')
     customer = pd.json_normalize(order_detail['customer']).add_prefix('customer_')
+    customer.to_csv('customer.csv')
     #TODO: Anonymize Customer
 
-    oh_cols = ['id', 'order_number', 'order_date', 'name', 'source_name', 
-        'confirmed', 'processing_method', 'financial_status', 'fulfillment_status',
+    oh_cols = ['id', 'order_number', 'name', 'source_name', 
+        'confirmed', 'financial_status', 'fulfillment_status',
         'created_at', 'processed_at', 'closed_at',
         'subtotal_price', 'total_price', 'total_tax', 'current_total_price', 
         'total_discounts', 'discount_codes',
@@ -45,15 +35,16 @@ def main():
     else:
         oh_write_disposition = "WRITE_EMPTY"
     
-    move.shop_to_gcs_bq(
-            api = "order_header",
-            api_res = order_header,
-            gcs_path = f"{utils.load_config()['gcs-orders']}order-header",
-            dataset = "orders",
-            table_name = "order_header",
-            write_disposition = oh_write_disposition,
-            partition = True,
-            partition_by = "created_at")
+    oh_move = move.shmover(api="order_header",
+        api_res=order_header,
+        gcs_path=gcs.GCSPath(f"{utils.load_config()['gcs-orders']}order-header"),
+        dataset="orders",
+        table_name="order_header",
+        write_disposition=oh_write_disposition,
+        partition=True,
+        partition_by="created_at") #created at is order date
+    
+    oh_move.shop_to_gcs()
 
 
 if __name__ == "__main__":
